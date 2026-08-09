d318c83383b21b55099984fe9177d5dbec81bf77
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2026-08-09 17:29:56 -0300

    chore(main,js): criar uma função inicial que trate os dados de pokemóns recebidos pela API em json

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,17 @@
-//document: o conteúdo de toda a página web
-const btnTeste = document.getElementById('btnTeste')
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-//Se houver um evento (no caso é um click), realize uma ação
-btnTeste.addEventListener("click", function () {
-  console.log("Clicou no botão.");
-})
+/*
+O Fetch fornece uma definição genérica de objetos de Request e Response (e outras coisas envolvidas com solicitações de rede). Isso permitirá que eles sejam usados onde quer que sejam necessários no futuro, seja para service workers, Cache API e outras coisas similares que manipulam ou modifiquem pedidos e respostas ou qualquer tipo de caso de uso que possa exigir que você gere suas próprias responses programaticamente.
+*/
+fetch(url)
+  .then((response) => response.json())        // Retorna a resposta da API em formato JSON
+  .then((jsonBody) => jsonBody.results)       // Retorna apenas os resultados dos dados encontrados no JSON
+  .then((pokemons) => {                       // Exibe os passos de como exibiu as informações dos    pokemons no log
+    debugger
+    console.log(pokemons)
+  })  
+  .catch((error) => console.log(error))
+  .finally(() => console.log('Requisição concluída'));
+
