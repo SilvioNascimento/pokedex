2ce7514efe26ce82d24993972c83cf149011f765
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2026-08-11 21:51:50 -0300

    refactor(main.js): mudar a linha de código para a linha 12

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,26 +16,23 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
   return `
-      <li class="pokemon">
-        <span class="number">#001</span>
+      <li class="pokemon ${pokemon.type}">
+        <span class="number">#${pokemon.number}</span>
         <span class="name">${pokemon.name}</span>
 
         <div class="detail">
           <ol class="types">
-            <li class="type">Glass</li>
-            <li class="type">Poison</li>
+            ${pokemon.types.map((type) => `<li class="type ${type.toLowerCase()}">${type}</li>`).join("")}
           </ol>
 
           <img
-            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png"
+            src="${pokemon.photo}"
             alt="${pokemon.name}"
           />
         </div>
       </li>
   `;
 }
-
-const pokemonList = document.getElementById('pokemonList');
 
 /*
 O Fetch fornece uma definição genérica de objetos de Request e Response (e outras coisas envolvidas com solicitações de rede). Isso permitirá que eles sejam usados onde quer que sejam necessários no futuro, seja para service workers, Cache API e outras coisas similares que manipulam ou modifiquem pedidos e respostas ou qualquer tipo de caso de uso que possa exigir que você gere suas próprias responses programaticamente.
