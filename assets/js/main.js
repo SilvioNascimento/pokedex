59b42008ed70cfec4ad5a11116330e6e57138974
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2026-08-11 21:52:49 -0300

    chore(main.js): modificar a exibição de pokemons para se adequar na paginação

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,12 +37,18 @@ function convertPokemonToLi(pokemon) {
 /*
 O Fetch fornece uma definição genérica de objetos de Request e Response (e outras coisas envolvidas com solicitações de rede). Isso permitirá que eles sejam usados onde quer que sejam necessários no futuro, seja para service workers, Cache API e outras coisas similares que manipulam ou modifiquem pedidos e respostas ou qualquer tipo de caso de uso que possa exigir que você gere suas próprias responses programaticamente.
 */
-pokeAPI.getPokemons().then((pokemons = []) => {
-  
-  pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('');
-  
-  //Ou pode substituir a linha 40 por estas linhas de código abaixo (está comentada)
-  // const newList = pokemons.map((pokemon) => convertPokemonToLi(pokemon));
-  // const newHTML = newList.join('');
-  // pokemonList.innerHTML += newHTML;
-});
+
+function loadPokemonItem(offset, limit) {
+  pokeAPI.getPokemons(offset, limit).then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join("");
+
+    //Ou pode substituir a linha 40 por estas linhas de código abaixo (está comentada)
+    // const newList = pokemons.map((pokemon) => convertPokemonToLi(pokemon));
+    // const newHTML = newList.join('');
+    // pokemonList.innerHTML += newHTML;
+  });
+}
+
+loadMoreButton.addEventListener('click', () => {
+  loadPokemonItem()
+})
