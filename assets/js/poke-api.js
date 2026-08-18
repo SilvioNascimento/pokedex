1885451e40fb91b35fffe32c9d77cbdc170aee02
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2026-08-18 12:03:13 -0300

    chore(poke-api.js): converter json em PokemonDetails antes de enviar sua resposta

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -56,5 +56,6 @@ pokeAPI.getPokemon = (id) => {
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
   return fetch(url)
     .then((response) => response.json())
+    .then((pokemon) => convertPokeApiDetailToPokemonDetails(pokemon))
     .catch((error) => console.error(error));
 };
